33e44d652244497db3d8d4946833f004bdd646b7
Blaser993 <corradodepinto.design@gmail.com>
2023-04-27 16:37:55 +0200

    PUSH PRE TICKET

---
+++ database/seeders/TagSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Tag;
+use Illuminate\Support\Str;
+
+class TagSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $tags = ['CSS','SASS', 'HTML', 'JS', 'Bootstrap', 'PHP', 'Vue','Laravel'  ];
+
+        foreach ($tags as $tag_name) {
+            $tag = new Tag();
+
+            $tag = $tag_name;
+            $tag->slug = Str::slug($tag_name);
+
+            $tag->save();
+        }
+    }
+}
